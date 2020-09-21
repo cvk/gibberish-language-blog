6e289621e9ad109369ca490a233597177074ca14
Chuck <umbra@mac.local>
2020-09-20 22:12:56 -0700

    start service worker

---
+++ sw.js
@@ -0,0 +1,56 @@
+---
+layout: null
+---
+
+const staticCacheName = "umbra-helper";
+
+console.log("installing service worker");
+
+const filesToCache = [
+  "/",
+  {% for page in site.html_pages %}
+    '{{ page.url }}',
+  {% endfor %}
+  {% for post in site.posts %}
+    '{{ post.url }}',
+  {% endfor %}
+  "/assets/umbra-social-icons.svg",
+  "/assets/minima-social-icons.svg",
+  "/gibberish-umbra-512-icon.png",
+  "/assets/main.css",
+  "/assets/css/style.css",
+  "/index.html"
+];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+
+  e.waitUntil(
+    caches.open(staticCacheName).then(function(cache){
+      return cache.addAll(filesToCache);
+    })
+  )
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(cacheNames){
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName.startsWith("gdad-s-river-static-")
+            && cacheName != staticCacheName;
+        }).map(function(cacheName){
+          return caches.delete(cacheName);
+        })
+      )
+    })
+  )
+});
+
+self.addEventListener("fetch", function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  )
+});
