322beedf1fd7491dbc41ce2e375a41cbaadd3a89
Chuck <umbra@mac.local>
2020-09-20 23:17:59 -0700

    more work on web app

--- sw.js
+++ sw.js
@@ -1,56 +1,58 @@
----
-layout: null
----
+var APP_PREFIX = 'GibberishUmbra_'     
+var VERSION = 'version_01'              
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                           
+  '/',                    
+  '/index.html'           
+]
 
-const staticCacheName = "umbra-helper";
-
-console.log("installing service worker");
-
-const filesToCache = [
-  "/",
-  {% for page in site.html_pages %}
-    '{{ page.url }}',
-  {% endfor %}
-  {% for post in site.posts %}
-    '{{ post.url }}',
-  {% endfor %}
-  "/assets/umbra-social-icons.svg",
-  "/assets/minima-social-icons.svg",
-  "/gibberish-umbra-512-icon.png",
-  "/assets/main.css",
-  "/assets/css/style.css",
-  "/index.html"
-];
-
-self.addEventListener("install", function(e){
-  self.skipWaiting();
-
-  e.waitUntil(
-    caches.open(staticCacheName).then(function(cache){
-      return cache.addAll(filesToCache);
-    })
-  )
-});
-
-self.addEventListener("activate", function(e){
-  e.waitUntil(
-    caches.keys().then(function(cacheNames){
-      return Promise.all(
-        cacheNames.filter(function(cacheName){
-          return cacheName.startsWith("umbra-helper-")
-            && cacheName != staticCacheName;
-        }).map(function(cacheName){
-          return caches.delete(cacheName);
-        })
-      )
-    })
-  )
-});
-
-self.addEventListener("fetch", function(e){
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
   )
-});
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
